dad86973b1873248dc3cefb7a51ad28fcc3965e0
Valentinor4687 <valentinor6@nycstudents.net>
2024-09-27 13:21:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Valentino Roque"; // update the part BETWEEN the "quotes"
+let userName = "Valentinor4687"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
